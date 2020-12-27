79cdceee197ce79e61148eba9023846be300ee03
Eric <ericbobcamargo@gmail.com>
2020-12-27 14:54:56 -0300

    Minor Changes

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import json
 from constants import *
 import gspread
+import os
 
 app = Flask(__name__)
 gc = gspread.service_account()
@@ -147,4 +148,5 @@ def make_df(d):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
