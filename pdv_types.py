70af0da0fd268b60c964d68d94f4f637d9c0dea7
Eric <ericbobcamargo@gmail.com>
2021-07-30 10:21:37 -0300

    Ajustes em PDVs e Abreviações

--- pdv_types.py
+++ pdv_types.py
@@ -7,23 +7,24 @@ PDV_TYPES = {
     'Medplex Santana': 'PDV Estoque',
     'NY205': 'PDV Estoque',
     'Prime Altos do Germânia': 'PDV Estoque',
-    'Prime Menino Deus': 'PDV Estoque',
-    'Prime Wallig': 'PDV Lançamento',
     'Prime Wish': 'PDV Estoque',
     'Quiosque Bourbon Ipiranga': 'PDV Estoque',
     'Sede Seller': 'Online',
     'Shopping Iguatemi': 'PDV Estoque',
     'Skyline': 'PDV Estoque',
     'Vintage Senior Residence': 'PDV Estoque',
+    'Vivaz Ecoville': 'PDV Estoque',
+    'Vivaz Santa Fé': 'PDV Estoque',
     'Yoo Moinhos': 'PDV Estoque',
-    'Zaffari Cabral': 'PDV Estoque'
 }
 
 PDVS_INATIVOS = {
     'The Arch': 'PDV Estoque',
     'Pirata Farnese': 'PDV Estoque',
+    'Prime Menino Deus': 'PDV Estoque',
+    'Prime Wallig': 'PDV Lançamento',
     'The Park': 'PDV Lançamento',
     'Quiosque Bodytech': 'PDV Estoque',
     'Axis Triple Business': 'PDV Estoque',
-
+    'Zaffari Cabral': 'PDV Estoque',
 }
